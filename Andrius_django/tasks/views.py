425a36924e2d9f015a384987f39d1953c82c1453
Andrius8777 <andrius001@protonmail.com>
2024-02-13 14:10:40 +0200

    frontend

--- Andrius_django/tasks/views.py
+++ Andrius_django/tasks/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from . import models
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    context = {
+        'projects_count': models.Project.objects.count(),
+        'task_count': models.Task.objects.count(),
+        'user_count': 
+    }
+    
 
 # Create your views here.
