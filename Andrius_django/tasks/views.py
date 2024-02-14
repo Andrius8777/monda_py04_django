84b0e549411814b4772c35d9f5b4e127f4ed7f7b
Andrius8777 <andrius001@protonmail.com>
2024-02-14 09:26:15 +0200

    html

--- Andrius_django/tasks/views.py
+++ Andrius_django/tasks/views.py
@@ -2,13 +2,10 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from . import models
 
-
 def index(request: HttpRequest) -> HttpResponse:
     context = {
         'projects_count': models.Project.objects.count(),
-        'task_count': models.Task.objects.count(),
-        'user_count': 
+        'tasks_count': models.Task.objects.count(),
+        'users_count': models.get_user_model().objects.count(),
     }
-    
-
-# Create your views here.
+    return render(request, 'tasks/index.html', context)
